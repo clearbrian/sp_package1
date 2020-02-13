115309cff34286bdafb9d46e0282c0fd665ec5da
Brian Clear <clearbrian@googlemail.com>
2020-02-13 17:54:50 +0000

    made class not open

--- Sources/sp_package1/MyClass.swift
+++ Sources/sp_package1/MyClass.swift
@@ -6,12 +6,20 @@
 //
 
 import Foundation
-open class MyClass{
+class MyClass{
+    
     //SP - must have a public init else client using the package cant find it
+    //let myClass = MyClass() THIS WILL FAIL
+    //initializer is inaccessible due to 'internal' protection level
     public init() {
         
     }
-    open func doSomething(){
+    
+    public func doSomething(){
         print("MyClass.doSomething() CALLED")
+    }
+    
+    func doSomethingNotPublic(){
+        print("MyClass.doSomethingNotPublic() CALLED")
     }
 }
