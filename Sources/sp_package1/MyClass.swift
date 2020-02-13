3f20ffedc7c46786332afdc35bcd24b9a4059601
Brian Clear <clearbrian@googlemail.com>
2020-02-13 17:47:56 +0000

    made init public

--- Sources/sp_package1/MyClass.swift
+++ Sources/sp_package1/MyClass.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 open class MyClass{
+    //SP - must have a public init else client using the package cant find it
+    public init() {
+        
+    }
     open func doSomething(){
         print("MyClass.doSomething() CALLED")
     }
